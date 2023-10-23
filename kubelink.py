6a87a8b266046ba668b72f72aa5b78d5f295f006
Udit Gaurav <udit.gaurav@harness.io>
2023-10-24 04:36:19 +0530

    update code

---
+++ kubelink.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+import requests
+import os
+
+app = Flask(__name__)
+
+GITHUB_REPO = 'uditgaurav/KubeLink'
+TOKEN = os.environ.get('GITHUB_TOKEN')
+API_URL = f'https://api.github.com/repos/{GITHUB_REPO}/dispatches'
+
+@app.route('/triggerClusterCreation', methods=['POST'])
+def trigger_cluster_creation():
+    # Define the event type for the GitHub Action
+    event_type = 'create-cluster'
+
+    headers = {
+        'Authorization': f'token {TOKEN}',
+        'Accept': 'application/vnd.github.everest-preview+json',
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        'event_type': event_type
+    }
+
+    response = requests.post(API_URL, headers=headers, json=data)
+
+    if response.status_code == 204:
+        return jsonify(status='in-progress')
+    else:
+        return jsonify(status='error', message=response.text), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
